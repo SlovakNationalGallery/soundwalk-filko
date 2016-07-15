2b2fef0164e38cd26e2909fbbe6c14d9124a056f
Philo van Kemenade <philovankemenade@gmail.com>
2016-07-15 14:49:18 +0200

    refactored i18n messages to separate JSON file

--- www/js/languageswitcher.js
+++ www/js/languageswitcher.js
@@ -1,19 +1,11 @@
 console.log("languageSwitcher!!!");
 
 LanguageSwitcher = {
-	languages: ["en", "sk"],
-	loadMessages: function() {
-		$.i18n().load( {
-			"en" : {
-			  "soundwalk-filko-title": "Filko EN",
-			  "soundwalk-filko-sub-title": "SoundWalk EN",
-			},
-			"sk" : {
-			  "soundwalk-filko-title": "Filko SK",
-			  "soundwalk-filko-sub-title": "SoundWalk SK",
-			}
-		} );
+	languages: ["sk", "en"],
+	loadMessages: function(messages) {
+		$.i18n().load( messages );
 	},
+	// set $.i18n locale if language is known
 	setLanguage: function(language) {
 		if ($.inArray(language, LanguageSwitcher.languages) > -1) {
 			$.i18n( {locale: language} );
@@ -22,8 +14,14 @@ LanguageSwitcher = {
 			console.log("unknown language "+language+" passed to setLanguage()");
 		}
 	},
+	// switch the language of a DOM element queried by [selector] to [language]
 	switchLanguage: function(selector, language) {
 		LanguageSwitcher.setLanguage(language);
 		$(selector).i18n();
 	}
 }
+
+$.getJSON( "i18n/messages.json?v="+new Date().getTime(), function( data ) {
+  console.log("getJSON success");
+  LanguageSwitcher.loadMessages(data);
+});
