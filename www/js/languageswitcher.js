c3e047a0d3eb88b1abb893e8fc00b7de3d881b5a
Philo van Kemenade <philovankemenade@gmail.com>
2016-07-20 10:40:46 +0200

    store user preferred language to native storage, retrieve the most preferred language on startup from native storage, device or fallback.

--- www/js/languageswitcher.js
+++ www/js/languageswitcher.js
@@ -8,9 +8,20 @@ LanguageSwitcher = {
 	setLanguage: function(language) {
 		if ($.inArray(language, LanguageSwitcher.languages) > -1) {
 			$.i18n( {locale: language} );
+			
+			// add to native storage
+			NativeStorage.setItem("filko-preferred-lang",language, 
+				function (setLang) {
+					// console.log("succesfully set preferred language to native storage: "+setLang);
+				}, 
+				function (error) {
+					console.log(error);
+					return false;
+				});
 		}
 		else {
 			console.log("unknown language "+language+" passed to setLanguage()");
+			return false;
 		}
 	},
 	// switch the language of a DOM element queried by [selector] to [language]
@@ -28,6 +39,62 @@ LanguageSwitcher = {
 		}
 		console.log("unknown preferred language value "+preferredLangValue+" passed to setLangFromGlobalizationPrefLang()");
 		return false;
+	},
+	// retrieve the most preferred user language
+	retrievePreferredLang: function () {
+		// 1. try previously stored value from Native Storage
+		// var storedLang = LanguageSwitcher.retrieveLangFromNativeStorage();
+		var storedLang;
+		NativeStorage.getItem("filko-preferred-lang", 
+			function (language) {
+				storedLang = language;
+				return;
+			}, 
+			function (error) {
+				console.log("error in NativeStorage.getItem() with code:");
+				console.log(error);
+				// NATIVE_WRITE_FAILED = 1
+				// ITEM_NOT_FOUND = 2
+				// NULL_REFERENCE = 3
+				// UNDEFINED_TYPE = 4
+				// JSON_ERROR = 5
+				// WRONG_PARAMETER = 6
+				return false;
+			}
+		);
+		if (storedLang) {
+			// console.log("setting storedLang: "+storedLang);
+			return storedLang;
+		}
+
+		// 2. get preferred language from device
+		var deviceLang;
+		navigator.globalization.getPreferredLanguage(
+	    	function (preferredLangObj) {
+	    		var preferredLangValue = preferredLangObj.value;
+	            for (var i = 0; i < LanguageSwitcher.languages.length; i++) {
+					if (preferredLangValue.startsWith(LanguageSwitcher.languages[i])) {
+						deviceLang = LanguageSwitcher.languages[i];
+						return;
+					}
+				}
+				console.log("unknown preferred language value "+preferredLangValue+" passed to setLangFromGlobalizationPrefLang()");
+				return false;
+	         },
+	      	function (error) { 
+	      		console.log(error);
+	      		return false;
+	      	}
+	  	);
+
+		if (deviceLang) {
+			// console.log("setting deviceLang: "+deviceLang);
+			return deviceLang;
+		}
+
+		// 3. fallback to fallbackLang
+		// console.log("setting fallbackLang: "+LanguageSwitcher.fallbackLang);
+		return LanguageSwitcher.fallbackLang;
 	}
 }
 
