f519dadc439a7f3e9cb76cf7ce1a28421133db53
Philo van Kemenade <philovankemenade@gmail.com>
2016-08-30 17:27:26 +0200

    reFix: make sure production events are tracked on production project

--- www/js/eventtracker.js
+++ www/js/eventtracker.js
@@ -6,12 +6,12 @@ var EventTracker = {
 	init: function (secretsPath) {
 		// Initialize using relevant token from secret .gitignore'd JSON file
 		// depending on whether app is run in browser (development) or phone (production)
-		$.getJSON(secretsPath, function(data) {        
-			var developmentPathname = "index.html";
-			if (window.location.pathname.toLowerCase().search(developmentPathname) < 0) {
-				var mpToken = data.mixpanelProductionToken;
-			} else {
+		$.getJSON(secretsPath, function(data) {
+			var developmentHosts = ["192.", "localhost"]
+			if (window.location.hostname.toLowerCase().search(developmentHosts[0]) > -1 || window.location.hostname.toLowerCase().search(developmentHosts[1]) > -1 ) {
 				var mpToken = data.mixpanelDevelopmentToken;
+			} else {
+				var mpToken = data.mixpanelProductionToken;
 			}
 			window.mixpanel.init(mpToken);
 			// track appOpened once mixpanel has been initialised
