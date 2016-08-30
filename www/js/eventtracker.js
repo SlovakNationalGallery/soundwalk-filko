7a86ee229422857af132cb37c8ba9efd2152bc27
Philo van Kemenade <philovankemenade@gmail.com>
2016-08-30 14:45:15 +0200

    track language as property of appOpened event

--- www/js/eventtracker.js
+++ www/js/eventtracker.js
@@ -1,4 +1,8 @@
-EventTracker = {
+var EventTracker = {
+	settings: {
+		DEBUG: true,
+    DISABLED: false,
+	},
 	init: function (secretsPath) {
 		// Initialize using relevant token from secret .gitignore'd JSON file
 		// depending on whether app is run in browser (development) or phone (production)
@@ -10,7 +14,23 @@ EventTracker = {
 				var mpToken = data.mixpanelDevelopmentToken;
 			}
 			window.mixpanel.init(mpToken);
-			window.mixpanel.track("appOpened");
+			// track appOpened once mixpanel has been initialised
+			EventTracker.track("appOpened", {});
 		});	
+	},
+	track: function (event, properties) {
+		if (EventTracker.settings.DISABLED) {
+			return;
+    };
+
+    $.extend(properties, {'language': $.i18n().locale});
+    
+    if (EventTracker.settings.DEBUG) {
+        console.log("******");
+        console.log("event: " + event);
+        console.log(properties);
+        console.log("/******");
+    };
+    window.mixpanel.track(event, properties);
 	}
 }
